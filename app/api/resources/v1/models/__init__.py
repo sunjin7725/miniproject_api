bb438e3c6fc0fc345a694cd76d17e3b8571e4fab
sunjin7725 <sunjin7725@gmail.com>
2023-02-14 15:08:14 +0900

    add: korea gpt2 model generator function added(/gpt)

--- app/api/resources/v1/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from api.resources.v1.models.stable_diffusion import (StableDiffusionV2Model, WaifuDiffusionModel,
-                                                      TrinArtModel, StableDiffusionModel, NAIModel)
-
-# sd_model = StableDiffusionV2Model()
-# wd_model = WaifuDiffusionModel()
-# trin_model = TrinArtModel()
-
-model = NAIModel()
