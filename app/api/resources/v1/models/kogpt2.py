10d38321761949a43b0f1da8fd9148ea0a5e2605
sunjin7725 <sunjin7725@gmail.com>
2023-02-14 16:50:55 +0900

    fix: kogpt2 error

--- app/api/resources/v1/models/kogpt2.py
+++ app/api/resources/v1/models/kogpt2.py
@@ -1,7 +1,6 @@
-import torch
-
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
 from config import MODEL_DIR
+
 
 tokenizer = PreTrainedTokenizerFast.from_pretrained(MODEL_DIR + '/kogpt2-base-v2',
                                                     bos_token='</s>', eos_token='</s>', unk_token='<unk>',
--- app/api/resources/v1/routers/kogpt2.py
+++ app/api/resources/v1/routers/kogpt2.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from common.utils import success_response
-from api.resources.v1.models.kogpt2 import tokenizer, model
+from api.resources.v1.models.kogpt2 import model, tokenizer
 router = APIRouter(
     prefix='/gpt',
     tags=['gpt']
@@ -9,14 +9,14 @@ router = APIRouter(
 
 
 @router.post("/")
-async def ocr_docs(text: str, max_length: int=128):
-    input_tensor = tokenizer.tokenize(text)
-    gen_tensor = model.generator(input_tensor,
-                                 max_length=max_length,
-                                 repetition_penalty=2.0,
-                                 pad_token_id=tokenizer.pad_token_id,
-                                 eos_token_id=tokenizer.eos_token_id,
-                                 bos_token_id=tokenizer.bos_token_id,
-                                 use_cache=True)
+async def ocr_docs(text: str, max_length: int = 128):
+    input_tensor = tokenizer.encode(text, return_tensors='pt')
+    gen_tensor = model.generate(input_tensor,
+                                max_length=max_length,
+                                repetition_penalty=2.0,
+                                pad_token_id=tokenizer.pad_token_id,
+                                eos_token_id=tokenizer.eos_token_id,
+                                bos_token_id=tokenizer.bos_token_id,
+                                use_cache=True)
     return success_response(tokenizer.decode(gen_tensor[0]))
 
